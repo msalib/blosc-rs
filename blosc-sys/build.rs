6b263f3bcb77147f1e85eebed3c05dc4dbe647df
Michael Salib <msalib@alum.mit.edu>
2021-04-03 08:59:26 -0400

    make static conditional

--- blosc-sys/build.rs
+++ blosc-sys/build.rs
@@ -1,6 +1,11 @@
 // vim: tw=80
+use std::env;
 
 fn main() {
     println!("cargo:rustc-link-search=native=/usr/local/lib");
-    println!("cargo:rustc-link-lib=static=blosc");
+    if env::var_os("BLOSC_STATIC").is_some() {
+        println!("cargo:rustc-link-lib=static=blosc");
+    } else {
+        println!("cargo:rustc-link-lib=blosc");
+    }
 }
