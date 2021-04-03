300e53ccd5b15a9830be794ed86f85abe5dbae96
Michael Salib <msalib@alum.mit.edu>
2021-04-03 09:08:29 -0400

    sigh

--- blosc-sys/build.rs
+++ blosc-sys/build.rs
@@ -2,7 +2,6 @@
 use std::env;
 
 fn main() {
-    println!("cargo:rustc-link-search=native=/usr/local/lib");
     if env::var_os("BLOSC_STATIC").is_some() {
         println!("cargo:rustc-link-lib=static=blosc");
     } else {
